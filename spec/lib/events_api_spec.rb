15478e926eb6459b5994817fa51ab8ef9cd8bc80
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-05-26 18:10:54 +0300

    Added specs for user_activity

--- spec/lib/events_api_spec.rb
+++ spec/lib/events_api_spec.rb
@@ -13,6 +13,33 @@ RSpec.describe EventsApi do
     it { is_expected.to be_a(Faraday::Connection) }
   end
 
+  describe '#metric_list_path' do
+    subject { instance.metric_list_path }
+    it { is_expected.to eq('api/v1/metrics') }
+  end
+
+  describe '#metric_list' do
+    subject { instance.metric_list }
+
+    specify do
+      VCR.use_cassette('events/metrics/index', erb: {
+                         base_url: Figaro.env.events_api_base_url }) do
+        is_expected.to eq([{ 'name' => 'Metric::ActiveUsers',
+                             'metric_name' => 'active_users',
+                             'type' => 'grouppable_by_timeframe' },
+                           { 'name' => 'Metric::MessagesSent',
+                             'metric_name' => 'messages_sent',
+                             'type' => 'grouppable_by_timeframe' },
+                           { 'name' => 'Metric::UsageByActiveUsers',
+                             'metric_name' => 'usage_by_active_users',
+                             'type' => 'grouppable_by_timeframe' },
+                           { 'name' => 'Metric::UserActivity',
+                             'metric_name' => 'user_activity',
+                             'type' => 'metric' }])
+      end
+    end
+  end
+
   describe '#metric_path' do
     subject { instance.metric_path(metric) }
     context 'messages_sent' do
@@ -30,70 +57,72 @@ RSpec.describe EventsApi do
 
       context 'group by day' do
         let(:options) { { group_by: :day } }
-        specify do
+        around do |example|
           VCR.use_cassette('events/metrics/messages_sent/by_day', erb: {
-                             base_url: Figaro.env.events_api_base_url
-                           }) do
-            is_expected.to eq(
-              '2015-05-09 00:00:00 UTC' => 3,
-              '2015-05-10 00:00:00 UTC' => 4,
-              '2015-05-11 00:00:00 UTC' => 5,
-              '2015-05-12 00:00:00 UTC' => 5)
-          end
+                             base_url: Figaro.env.events_api_base_url }) { example.run }
+        end
+
+        specify do
+          is_expected.to eq(
+            '2015-05-09 00:00:00 UTC' => 3,
+            '2015-05-10 00:00:00 UTC' => 4,
+            '2015-05-11 00:00:00 UTC' => 5,
+            '2015-05-12 00:00:00 UTC' => 5)
         end
       end
 
       context 'group by week' do
         let(:options) { { group_by: :week } }
-        specify do
+        around do |example|
           VCR.use_cassette('events/metrics/messages_sent/by_week', erb: {
-                             base_url: Figaro.env.events_api_base_url
-                           }) do
-            is_expected.to eq(
-              '2015-05-04 00:00:00 UTC' => 7,
-              '2015-05-11 00:00:00 UTC' => 10)
-          end
+            base_url: Figaro.env.events_api_base_url }) { example.run }
+        end
+
+        specify do
+          is_expected.to eq(
+            '2015-05-04 00:00:00 UTC' => 7,
+            '2015-05-11 00:00:00 UTC' => 10)
         end
       end
 
       context 'group by month' do
         let(:options) { { group_by: :month } }
-        specify do
+        around do |example|
           VCR.use_cassette('events/metrics/messages_sent/by_month', erb: {
-                             base_url: Figaro.env.events_api_base_url
-                           }) do
-            is_expected.to eq(
-              '2015-05-01 00:00:00 UTC' => 17)
-          end
+                             base_url: Figaro.env.events_api_base_url }) { example.run }
+        end
+
+        specify do
+          is_expected.to eq('2015-05-01 00:00:00 UTC' => 17)
         end
       end
     end
-  end
 
-  describe '#metric_list_path' do
-    subject { instance.metric_list_path }
-    it { is_expected.to eq('api/v1/metrics') }
-  end
+    describe 'user_activity' do
+      let(:metric) { :user_activity }
+      let(:options) {}
+      let(:user_id) { 'RxDrzAIuF9mFw7Xx9NSM' }
 
-  describe '#metric_list' do
-    subject { instance.metric_list }
+      context 'user_id' do
+        let(:options) { { user_id: user_id } }
+        around do |example|
+          VCR.use_cassette('events/metrics/user_activity/with_user_id', erb: {
+                             base_url: Figaro.env.events_api_base_url,
+                             user_id: user_id }) { example.run }
+        end
 
-    specify do
-      VCR.use_cassette('events/metrics/index', erb: {
-                         base_url: Figaro.env.events_api_base_url
-                       }) do
-        is_expected.to eq([{ 'name' => 'Metric::ActiveUsers',
-                             'metric_name' => 'active_users',
-                             'type' => 'grouppable_by_timeframe' },
-                           { 'name' => 'Metric::MessagesSent',
-                             'metric_name' => 'messages_sent',
-                             'type' => 'grouppable_by_timeframe' },
-                           { 'name' => 'Metric::UsageByActiveUsers',
-                             'metric_name' => 'usage_by_active_users',
-                             'type' => 'grouppable_by_timeframe' },
-                           { 'name' => 'Metric::UserActivity',
-                             'metric_name' => 'user_activity',
-                             'type' => 'metric' }])
+        it { is_expected.to be_a(Array) }
+      end
+
+      context 'user_activity is empty' do
+        around do |example|
+          VCR.use_cassette('events/metrics/user_activity/without_user_id', erb: {
+                             base_url: Figaro.env.events_api_base_url }) { example.run }
+        end
+
+        specify do
+          expect { subject }.to raise_error(Faraday::ClientError)
+        end
       end
     end
   end
