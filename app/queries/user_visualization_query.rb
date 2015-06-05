b5ea491822b8dd94560973e952dac0ef0f3020c2
Ivan Kornilov <vano468@gmail.com>
2015-06-05 13:48:54 +0300

    Implement optimized counting of connection counts

--- app/queries/user_visualization_query.rb
+++ app/queries/user_visualization_query.rb
@@ -7,7 +7,7 @@ class UserVisualizationQuery
 
   def execute
     @connections = Connection.for_user_id(user.id).includes(:creator).includes(:target)
-    @friends = User.where id: get_friends_ids
+    @friends = friends_with_connection_counts
   end
 
 private
@@ -17,5 +17,18 @@ private
       memo << conn.target_id  if user.id != conn.target_id
       memo << conn.creator_id if user.id != conn.creator_id
     end.uniq
+  end
+
+  def friends_with_connection_counts
+    query = <<-SQL
+      SELECT users.*, COUNT(connections.id) AS connection_counts
+      FROM users
+      INNER JOIN connections ON
+        users.id = connections.target_id OR
+        users.id = connections.creator_id
+      WHERE users.id IN ?
+      GROUP BY users.id
+    SQL
+    User.find_by_sql [query, get_friends_ids]
   end
 end
--- app/serializers/user_visualization_serializer.rb
+++ app/serializers/user_visualization_serializer.rb
@@ -1,3 +1,7 @@
 class UserVisualizationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :mobile_number, :status
+  attributes :id, :name, :mobile_number, :status, :connection_counts
+
+  def connection_counts
+    object.respond_to?(:connection_counts) ? object.connection_counts : 0
+  end
 end
