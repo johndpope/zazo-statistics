96b404be99a45d4970609ae70a678ddb9be846f8
Ivan Kornilov <vano468@gmail.com>
2015-06-11 15:00:23 +0300

    Implement ConnectionsAndUsersInDepth query

---
+++ app/queries/connections_and_users_in_depth_query.rb
@@ -0,0 +1,66 @@
+class ConnectionsAndUsersInDepthQuery
+  attr_reader :connections, :users
+
+  def initialize(user, depth)
+    @user  = user
+    @depth = depth
+    @users = []
+    @connections = {}
+  end
+
+  def execute
+    depth = @depth - 1
+    get_and_store_users @user.id, @user.mkey,
+                        get_and_store_connections(@user.id)
+    while depth > 0
+      copy_accumulator.each do |user|
+        get_and_store_users user[:id], user[:mkey],
+                            get_and_store_connections(user[:id])
+      end
+      depth -= 1
+    end
+    stretch_users
+  end
+
+  private
+
+  def get_and_store_connections(user_id)
+    connections = Connection.for_user_id(user_id).includes(:creator).includes(:target)
+    @connections[@user.id] = connections
+  end
+
+  def get_and_store_users(user_id, user_mkey, connections)
+    users = get_users_ids({ id: user_id, mkey: user_mkey }, connections)
+    accumulate_users users
+    @users << users
+  end
+
+  def accumulate_users(ids)
+    @accumulator ||= []
+    @accumulator += ids[:friends]
+  end
+
+  def copy_accumulator
+    copy = @accumulator
+    @accumulator = []
+    copy
+  end
+
+  def stretch_users
+    @users = @users.each_with_object([]) do |pack, memo|
+      memo << pack[:target]
+      pack[:friends].each { |u| memo << u }
+    end.uniq
+  end
+
+  def get_users_ids(user_data, connections)
+    users_ids = connections.each_with_object([]) do |conn, memo|
+      memo << { id: conn.target_id,  mkey: conn.target.event_id }  if user_data[:id] != conn.target_id
+      memo << { id: conn.creator_id, mkey: conn.creator.event_id } if user_data[:id] != conn.creator_id
+    end.uniq
+    {
+      target: { id: user_data[:id], mkey: user_data[:mkey] },
+      friends: users_ids
+    }
+  end
+end
