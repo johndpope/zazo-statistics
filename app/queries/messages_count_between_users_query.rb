6231cb3c04529eb775177ded13e90b83e19e75aa
Ivan Kornilov <vano468@gmail.com>
2015-06-12 12:52:08 +0300

    Implement social graph building by depth GET param

--- app/queries/messages_count_between_users_query.rb
+++ app/queries/messages_count_between_users_query.rb
@@ -1,12 +1,19 @@
 class MessagesCountBetweenUsersQuery
-  def initialize(user_mkey, friends_ids)
-    @user_mkey   = user_mkey
-    @friends_ids = friends_ids.map { |f| f[:mkey] }
+  def initialize(users_packs)
+    @users = prepare_users users_packs
   end
 
   def execute
     EventsApi.new.metric_data :messages_count_between_users,
-                              user_id: @user_mkey,
-                              friends_ids: @friends_ids
+                              users_ids: @users
+  end
+
+  private
+
+  def prepare_users(packs)
+    packs.each_with_object({}) do |user_pack, memo|
+      friends = user_pack[:friends].map { |f| f[:mkey] }
+      memo[user_pack[:target][:mkey]] = friends
+    end
   end
 end
--- app/queries/user_visualization_data_query.rb
+++ app/queries/user_visualization_data_query.rb
@@ -4,29 +4,29 @@ class UserVisualizationDataQuery
   def initialize(user, settings = {})
     @user     = user
     @settings = {
-      depth: settings.has_key?(:depth) ? settings[:depth].to_i : 1
+      depth: settings.key?(:depth) ? settings[:depth].to_i : 1
     }
     @in_depth = ConnectionsAndUsersInDepthQuery.new user, @settings[:depth]
   end
 
   def execute
     @in_depth.execute
-    @connections = get_connections
-    @users  = get_users
+    @connections = serialized_connections
+    @users       = serialized_users
     @target = @users.find { |u| u[:id] == @user.id }
   end
 
   private
 
-  def get_users
+  def serialized_users
     users    = UsersWithConnectionCountsQuery.new(@in_depth.stretched_users).execute
     users    = [target] if users.empty?
     messages = AverageMessagesByPeriodQuery.new(users).execute
     UsersVisualizationSerializer.new(users, messages: messages).serialize
   end
 
-  def get_connections
-    #counts = MessagesCountBetweenUsersQuery.new(@user.mkey, get_users_ids(false, connections)).execute
-    ConnectionsVisualizationSerializer.new(@in_depth.connections, counts: []).serialize
+  def serialized_connections
+    counts = MessagesCountBetweenUsersQuery.new(@in_depth.users).execute
+    ConnectionsVisualizationSerializer.new(@in_depth.connections, counts: counts).serialize
   end
 end
