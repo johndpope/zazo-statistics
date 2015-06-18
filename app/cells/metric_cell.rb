6a346573bb0f108efda0bc43eb14737bc69fe0bd
Ivan Kornilov <vano468@gmail.com>
2015-06-18 15:43:35 +0300

    Refactor metrics charts with ViewModel

---
+++ app/cells/metric_cell.rb
@@ -0,0 +1,38 @@
+class MetricCell < Cell::ViewModel
+  include Chartkick::Helper
+
+  layout :_layout
+  property :type
+  property :name
+
+  def show
+    render type
+  rescue Cell::TemplateMissingError
+    render :show
+  end
+
+  private
+
+  def title
+    name.titleize
+  end
+
+  def chart(options = {})
+    send type, options
+  end
+
+  def aggregated_by_timeframe(options)
+    area_chart url_for(action: :show, id: name, group_by: options[:group_by], only_path: true),
+               id: chart_id
+  end
+
+  def onboarding_info(*)
+    data = EventsApi.new.metric_data :onboarding_info
+    data = data.keys.map { |key| { name: key, data: data[key] } }
+    line_chart data, height: '800px', min: -5, max: 100, id: chart_id
+  end
+
+  def chart_id
+    "chart-#{SecureRandom.hex}"
+  end
+end
