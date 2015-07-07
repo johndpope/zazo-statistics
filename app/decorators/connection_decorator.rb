dfd6b2048641d5f77161c8ec35fd461964569add
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-07-07 16:15:04 +0300

    :beetle: Fixed wrong status on connected users

--- app/decorators/connection_decorator.rb
+++ app/decorators/connection_decorator.rb
@@ -4,12 +4,11 @@ class ConnectionDecorator < Draper::Decorator
   def direction_arrow(user_id)
     object.creator.id == user_id ? '<- (target)' : '-> (creator)'
   end
-  
+
   def friend_info(user_id)
     connected_user = object.connected_user(user_id)
     h.link_to "#{connected_user.name} (#{connected_user.mobile_number})", connected_user
   end
-
 
   def status(user)
     new_status = object.active? ? :active : user.status
