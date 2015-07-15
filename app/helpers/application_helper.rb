4f0709ad627bb66f67b987180d3aaddf892fbb86
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2015-07-15 17:48:44 +0300

    :star: Added view for *messages_failures*

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def human_device_platform(platform)
     t(platform, scope: :device_platforms)
   end
+
+  def part_and_percentage(total, part)
+    number = total.zero? ? 0 : part * 100.0 / total
+    percent = number_to_percentage(number, precision: 2)
+    "#{part} (#{percent})"
+  end
 end
