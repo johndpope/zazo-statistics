0abbf6df7f53717073b64568d0e4382b7d7e4c1a
Ivan Kornilov <vano468@gmail.com>
2015-08-24 18:22:38 +0300

    Implement GrowthCalculator UI

--- app/concepts/metric/growth_calculator/cell.rb
+++ app/concepts/metric/growth_calculator/cell.rb
@@ -5,9 +5,43 @@ class Metric::GrowthCalculator::Cell < Cell::Concept
 
   private
 
+  #
+  # attributes
+  #
+
   def nmv_sent_invite_rate
-    count = model['verified_sent_invitations']['verified_sent_invitations'].to_f
-    total = model['verified_sent_invitations']['total_verified'].to_f
-    (count / total * 100).round 2
+    rate 'verified_sent_invitations', 'total_verified', 'verified_sent_invitations'
+  end
+
+  def total_invites_per_nmv
+    model['average_invitations_count'].find do |row|
+      row['week_after_verified'] == 'total'
+    end['avg_invitations_count'].to_f.round 2
+  end
+
+  def invited_to_registered_rate
+    rate 'invited_to_registered', 'total_invited', 'invited_that_register'
+  end
+
+  def nmr_to_nmv_rate
+    rate 'registered_to_verified', 'total_registered', 'registered_that_verify'
+  end
+
+  def delay_in_days
+    [
+      { keys: %w(verified_sent_invitations avg_delay_in_hours), convert: -> (val) { val.to_f / 24 } },
+      { keys: %w(invited_to_registered avg_delay_in_hours),     convert: -> (val) { val.to_f / 24 } },
+      { keys: %w(registered_to_verified avg_delay_in_minutes),  convert: -> (val) { val.to_f / 24 / 60 } }
+    ].inject(0) { |sum, row| sum + row[:convert].call(model[row[:keys][0]][row[:keys][1]]) }.round 2
+  end
+
+  #
+  # helpers
+  #
+
+  def rate(category_key, total_key, count_key)
+    total = model[category_key][total_key]
+    count = model[category_key][count_key]
+    (count.to_f / total.to_f).round 2
   end
 end
